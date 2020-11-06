e22ba64f1caa2b252a3323c0be0f4a6c9d2185da
mayanagarwal <mayanagarwal@ymail.com>
2020-11-06 15:45:10 +1100

    80 20 split

--- script.py
+++ script.py
@@ -2,16 +2,15 @@ import json
 import os
 import random
 
-a = 0
 
-fin1 = open("./custom_train/trainfs.txt", "a+")
-fin2 = open("./custom_train/validfs.txt", "a+")
+fin1 = open("./custom_train/train_final.txt", "a+")
+fin2 = open("./custom_train/valid_final.txt", "a+")
 
-my_randoms = random.sample(range(0, 13000), 10400)
-for file in os.listdir("../yolov3/custom_data/FaceSwap/images/"):
-	a += 1
-	path = os.path.join("../yolov3/custom_data/FaceSwap/images/",file)
-	if a in my_randoms:  
+my_randoms = random.sample(range(0, 1000), 800)
+for file in os.listdir("../yolov3/custom_data/deepfake/images/"):
+	path = os.path.join("../yolov3/custom_data/deepfake/images/",file)
+	split = file.split("_")
+	if int(split[1]) in my_randoms:  
 		fin1.write(path + "\n")
 	else:
 		fin2.write(path + "\n")
